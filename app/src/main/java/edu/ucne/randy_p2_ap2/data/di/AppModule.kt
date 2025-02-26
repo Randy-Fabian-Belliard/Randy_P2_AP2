7f550e8d9a12445a600dd104ffd2532c8f6dfa83
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-26 11:23:25 -0400

    Creando AppModule

---
+++ app/src/main/java/edu/ucne/randy_p2_ap2/data/di/AppModule.kt
@@ -0,0 +1,20 @@
+package edu.ucne.randy_p2_ap2.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+}
