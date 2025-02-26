56f420e97b4b05616806df5e4c6df17882fdc1ea
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-26 10:57:34 -0400

    Inicial

---
+++ app/src/main/java/edu/ucne/randy_p2_ap2/MainActivity.kt
@@ -0,0 +1,31 @@
+package edu.ucne.randy_p2_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.randy_p2_ap2.ui.theme.Randy_P2_AP2Theme
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Randy_P2_AP2Theme {
+
+                }
+            }
+        }
+}
+
+
---
+++ app/src/main/java/edu/ucne/randy_p2_ap2/ui/theme/RandyP2AP2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.randy_p2_ap2.ui.theme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RandyP2AP2App(): Application() {
+
+}
