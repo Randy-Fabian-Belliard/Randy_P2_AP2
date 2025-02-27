795cb6b4b63ee42dc9b5c753b7a159e700277ccf
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-26 21:05:03 -0400

    Modifiando depositosapi

--- app/src/main/java/edu/ucne/randy_p2_ap2/data/remote/a/DepositosApi.kt
+++ app/src/main/java/edu/ucne/randy_p2_ap2/data/remote/a/DepositosApi.kt
@@ -4,15 +4,19 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface DepositosApi {
-
+    @Headers("X-API-Key:test")
     @GET("api/Depositos/{id}")
     suspend fun getDeposito(@Path("id") id: Int): DepositosDto
+    @Headers("X-API-Key:test")
     @GET("api/Depositos")
     suspend fun getDeposito(): List<DepositosDto>
+    @Headers("X-API-Key:test")
     @POST("api/Depositos")
     suspend fun saveDeposito(@Body tepositosDto: DepositosDto?):DepositosDto?
+    @Headers("X-API-Key:test")
     @PUT("api/Depositos/{id}")
     suspend fun updateDeposito(@Path("id") id: Int, @Body ticketDto: DepositosDto?): Response<DepositosDto>
+    @Headers("X-API-Key:test")
     @DELETE("api/Depositos/{id}")
     suspend fun deleteDeposito(@Path("id") id: Int): Response<Unit>
 
