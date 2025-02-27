f2851cf86fc846f62e6b7c38be51230242680549
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-26 20:28:25 -0400

    Modificando AppModule

--- app/src/main/java/edu/ucne/randy_p2_ap2/data/di/AppModule.kt
+++ app/src/main/java/edu/ucne/randy_p2_ap2/data/di/AppModule.kt
@@ -9,12 +9,33 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.randy_p2_ap2.data.remote.a.DepositosApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
+
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+@Provides
+@Singleton
+fun providesMoshi(): Moshi =
+    Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesTicketApi(moshi: Moshi): DepositosApi {
+        return Retrofit.Builder()
+            .baseUrl("https://sagapi-dev.azurewebsites.net/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DepositosApi::class.java)
+    }
+
+
 
 }
