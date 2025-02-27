d5f7f72e14b6b2d9bf6eebfd0dd8a06147211f8e
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-02-26 20:37:21 -0400

    Creando Modificando MainActivity

--- app/src/main/java/edu/ucne/randy_p2_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/randy_p2_ap2/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.randy_p2_ap2.Presentation.Navigation.AppNavHost
 import edu.ucne.randy_p2_ap2.ui.theme.Randy_P2_AP2Theme
 
 
@@ -15,11 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Randy_P2_AP2Theme {
-
-
-                }
+                val navHost = rememberNavController()
+                AppNavHost(
+                    navHost
+                )
             }
         }
+    }
 }
 
 
