ac1fcdd89bc5d9436cdc910019aaaa179fd7c623
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-03-03 14:57:57 -0400

    Modificando las funciones de la api

--- app/src/main/java/edu/ucne/randy_p2_ap2/data/remote/a/DepositosApi.kt
+++ app/src/main/java/edu/ucne/randy_p2_ap2/data/remote/a/DepositosApi.kt
@@ -7,17 +7,25 @@ interface DepositosApi {
     @Headers("X-API-Key:test")
     @GET("api/Depositos/{id}")
     suspend fun getDeposito(@Path("id") id: Int): DepositosDto
+
     @Headers("X-API-Key:test")
     @GET("api/Depositos")
-    suspend fun getDeposito(): List<DepositosDto>
+    suspend fun getDepositos(): List<DepositosDto>
+
     @Headers("X-API-Key:test")
     @POST("api/Depositos")
-    suspend fun saveDeposito(@Body tepositosDto: DepositosDto?):DepositosDto?
+    suspend fun saveDeposito(@Body depositosDto: DepositosDto): Response<DepositosDto>
+
     @Headers("X-API-Key:test")
     @PUT("api/Depositos/{id}")
-    suspend fun updateDeposito(@Path("id") id: Int, @Body ticketDto: DepositosDto?): Response<DepositosDto>
+    suspend fun updateDeposito(@Path("id") id: Int, @Body depositosDto: DepositosDto?): Response<DepositosDto>
+
     @Headers("X-API-Key:test")
     @DELETE("api/Depositos/{id}")
     suspend fun deleteDeposito(@Path("id") id: Int): Response<Unit>
 
 }
+
+
+
+
